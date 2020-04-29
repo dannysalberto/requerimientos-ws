90b080cae5d14ca743d0235d79a288cf7511707d
Jhon Eduard Ortiz Sanchez <jhon.ortiz@interkont.co>
2020-04-29 18:59:25 -0500

    Servicios para obtener datos reales de actividades de proyecto (3)

--- src/main/java/co/com/interkont/wscobra/dao/ActividadesRepository.java
+++ src/main/java/co/com/interkont/wscobra/dao/ActividadesRepository.java
@@ -6,10 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import co.com.interkont.wscobra.dto.VistaActividades;
-import co.com.interkont.wscobra.dto.VistaProyectosLista;
 
 @Repository
-public interface ActividadesRepository extends JpaRepository<VistaProyectosLista, Integer>{
+public interface ActividadesRepository extends JpaRepository<VistaActividades, Integer>{
 	
 	
 	public List<VistaActividades> findByCodigoProyecto(Integer codigoProyecto);
